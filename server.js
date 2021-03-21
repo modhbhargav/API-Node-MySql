e5f2ae13696bf676f9fbf6b63cc00df8b12b42ac
Bhargav Patel <bpatel12123@gmail.com>
2021-03-21 17:31:57 +0530

    added comment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app")
 
 // set  port number
 const port = process.env.PORT || 5001;
-
+// server lis
 const server = http.createServer(app);
 server.listen(port, () => {
     console.log(`Server is runing on port ${port}`);
